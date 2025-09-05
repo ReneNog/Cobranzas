33d28b8e9b1c5092e17e614eda817b851d964374
ReneNo <elrenenoguera@gmail.com>
2025-09-05 07:57:52 -0400

    primer commit

---
+++ AppCobranzas/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes } from "react-router-dom"
+
+
+
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+    <Routes>
+    <h1>Hola</h1>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
